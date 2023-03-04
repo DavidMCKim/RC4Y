00ffd243856c9e461a2532409256fa95cc3e624c
DavidMCKim <kimminchang96@gmail.com>
2023-03-04 19:52:08 +0900

    db관련 소스 수정

--- crawl_camping_info.py
+++ crawl_camping_info.py
@@ -1,16 +1,19 @@
 import re
+import json
 import requests
 import pandas as pd
 from loguru import logger
 from bs4 import BeautifulSoup
+from db.dbconnector import DBConnector
 
-class CrawlCampingInfo():
+class CrawlCampsiteInfo():
     def __init__(self):
-        self.url = 'https://pcmap.place.naver.com/accommodation/list?query=<%keyword%>&mapUrl=https://map.naver.com/v5/search/<%keyword%>'
+        self.url     = 'https://pcmap.place.naver.com/accommodation/list?query=<%keyword%>&mapUrl=https://map.naver.com/v5/search/<%keyword%>'
         self.baseUrl = 'https://www.map.naver.com/v5/search/<%keyword%>'
+        self.db      = DBConnector()
         
         
-    def Crawl_Camping_Info(self):
+    def Init_RC4Y(self):
         """ crawl start date, crawl end date 사이에 있는 뉴스 링크 수집 """
         try:
             url = self.url.replace('<%keyword%>', '경기도 캠핑장')
@@ -32,16 +35,51 @@ class CrawlCampingInfo():
 
             html = request.text
             soup = BeautifulSoup(html, "html.parser")
-            type = str(soup.find_all("script")[2])
-            test = re.compile('^window.__APOLLO_STATE__ = ')
-            print(test.findall(type))
-            
+            # split이 위험할 수도 있으니 정규표현식으로 변경할 수 있도록 수정 필요!!
+            html_campsite_info = str(soup.find_all("script")[2]).split(';')[4]
+            html_campsite_info = f"{html_campsite_info.replace('window.__APOLLO_STATE__ = ','')}"
+            json_campsite_info = json.loads(html_campsite_info)
+            self.Get_Campsite_Info(json_campsite_info)
+               
+        except Exception as e:
+            print(e)
 
+    def Get_Campsite_Info(self, json_campsite_info):
+        try:
+            scrap_data = {'Category' : '', 'CategoryCode' : '', 'CampingId' : '', 'CampSiteName' : '', 'RoadAddress' : '', 'Lotaddress' : '', 'Latitude' : '',
+                          'Longitude' : '', 'PhoneNumber' : '', 'VirtualNumber' : '', 'PromotionTitle' : '', 'Facility' : '', 'ImageUrls' : '', 'UseFlag' : ''}
+            keyType = re.compile('^AccommodationSummary')
+            for key in json_campsite_info.keys():
+                if keyType.match(key):
+                    print(json_campsite_info[key])
+                    campsiteInfo = json_campsite_info[key]
+                    try:
+                        scrap_data['Category'] = campsiteInfo['businessCategory']
+                        scrap_data['CategoryCode'] = campsiteInfo['categoryCode']
+                        scrap_data['CampingId'] = campsiteInfo['id']
+                        scrap_data['CampSiteName'] = campsiteInfo['name']
+                        scrap_data['RoadAddress'] = campsiteInfo['roadAddress']
+                        scrap_data['Lotaddress'] = campsiteInfo['commonAddress'] + campsiteInfo['address']
+                        scrap_data['Latitude'] = campsiteInfo['x']
+                        scrap_data['Longitude'] = campsiteInfo['y']
+                        scrap_data['PhoneNumber'] = campsiteInfo['phone']
+                        scrap_data['VirtualNumber'] = campsiteInfo['virtualPhone']
+                        scrap_data['PromotionTitle'] = campsiteInfo['promotionTitle']
+                        scrap_data['Facility'] = campsiteInfo['facility']
+                        scrap_data['ImageUrls'] = campsiteInfo['imageUrls']
+                        scrap_data['UseFlag'] = 'Y'
 
+                        self.db.select('''
+                                        select *
+                                        from tb_Campsite_Info
+                                       ''')
+
+                    except Exception as e:
+                        print(e)
         except Exception as e:
             print(e)
 
 if __name__ == '__main__':
-    c = CrawlCampingInfo()
-    c.Crawl_Camping_Info()
+    c = CrawlCampsiteInfo()
+    c.Init_RC4Y()
 
--- db/dbconnector.py
+++ db/dbconnector.py
@@ -2,66 +2,13 @@ import asyncio
 import aiomysql
 from mysql.connector import pooling
 
-import logging
-
-logger = logging.getLogger('db')
-logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter(u'[%(asctime)s] [%(levelname)s] :: %(message)s')
-file_handler = logging.FileHandler(filename = './ccnd_web_api.log', encoding = 'utf-8')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-    
 class DBConnector :
     def __init__(self) :
-        ##### docker
-        # self.db = mysql.connector.connect(
-        #         host="203.245.41.195",
-        #         user="ccnd",
-        #         password="ccnd123",
-        #      l   database="ccnd" 
-        # )
-        # self.db = pooling.MySQLConnectionPool(
-        #         pool_size = 5,
-        #         pool_name = 'ccndpool',
-        #         host="203.245.41.195",
-        #         user="ccnd",
-        #         password="ccnd123",
-        #         database="ccnd" 
-        # )
-
-        ##### rfc SERVER
-        # self.db = mysql.connector.connect(
-        #         host="121.124.126.43",
-        #         user="root",
-        #         password="Rlaguscjf13@$",
-        #         database="DATA_MART_CN",
-        # )
-        # self.db = pooling.MySQLConnectionPool(
-        #         pool_size = 15,
-        #         pool_name = 'ccndpool',
-        #         host="121.124.126.43",
-        #         user="root",
-        #         password="Rlaguscjf13@$",
-        #         database="DATA_MART_CN" 
-        # )
-        
-        #### SERVER private
-        # self.db = pooling.MySQLConnectionPool(
-        #         pool_size = 15,
-        #         pool_name = 'ccndpool',
-        #         host="172.30.1.222",
-        #         user="cu",
-        #         password="Rlaguscjf13@$",
-        #         database="CCND_DATA" 
-        # )
-
         # #### SERVER
         self.db = pooling.MySQLConnectionPool(
                 pool_size = 15,
                 pool_name = 'rc4ypool',
                 host="localhost",
-                user="root",
-                password="Mckim1996!@#$",
                 database="rc4y" 
         )
 
@@ -69,7 +16,6 @@ class DBConnector :
         result = None
         try :
             conn = self.db.get_connection()
-            logger.debug(f'Connected to Database using Pool >> {conn}')
             cursor = conn.cursor()
 
             cursor.execute(query)
@@ -80,7 +26,7 @@ class DBConnector :
                 result.append(list(r))
 
         except Exception as ex :
-            logger.error(f'/select >> {ex}')
+            print(ex)
 
         finally:
             if conn.is_connected() or conn.pool_name is not None :
@@ -93,7 +39,6 @@ class DBConnector :
         result = -1
         try :
             conn = self.db.get_connection()
-            logger.debug(f'Connected to Database using Pool >> {conn}')
             cursor = conn.cursor()
 
             cursor.execute(query)
@@ -102,7 +47,7 @@ class DBConnector :
             result = cursor.rowcount
 
         except Exception as ex :
-            logger.error(f'/select >> {ex}')
+            print(ex)
 
         finally:
             if conn.is_connected() or conn.pool_name is not None :
@@ -115,7 +60,6 @@ class DBConnector :
         result = -1
         try :
             conn = self.db.get_connection()
-            logger.debug(f'Connected to Database using Pool >> {conn}')
             cursor = conn.cursor()
 
             value_tmp = str(tuple(('%s' for i in range (0, len(column))))).replace('\'', '')
@@ -131,7 +75,7 @@ class DBConnector :
             result = cursor.rowcount
 
         except Exception as ex :
-            logger.error(f'/select >> {ex}')
+            print(ex)   
 
         finally:
             if conn.is_connected() or conn.pool_name is not None :
@@ -144,7 +88,6 @@ class DBConnector :
         result = -1
         try:
             conn = self.db.get_connection()
-            logger.debug(f'Connected to Database using Pool >> {conn}')
             cursor = conn.cursor()
 
             cursor.execute(query)
@@ -153,7 +96,7 @@ class DBConnector :
             result = cursor.rowcount
         
         except Exception as ex :
-            logger.error(f'/select >> {ex}')
+            print(ex)
 
         finally:
             if conn.is_connected() or conn.pool_name is not None :
